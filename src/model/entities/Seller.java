1dfe81b97ebfc77750fae3064512cf5ea0124393
Gustavo Rossi <toxic200203@gmail.com>
2023-08-21 16:01:38 -0300

    jdbc

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,18 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Seller{
+
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+
+    private Department department;
+
+    public Seller() {}
+
+
+}
